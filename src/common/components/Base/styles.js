266eec1960527f8ab63857736f89c8cc146d831b
Jordan MacBook <suarez.jordan@orange.fr>
2021-02-25 10:35:16 +0100

    Fix: fix email message validation/ fix scroll on firefox browser.

--- src/common/components/Base/styles.js
+++ src/common/components/Base/styles.js
@@ -3,7 +3,7 @@ import background from 'common/assets/images/background.jpg'
 export default () => ({
   root: {
     height: '100vh',
-    overflow: 'hidden',
+    overflow: 'scroll',
     backgroundImage: `url(${background})`,
     backgroundPosition: 'top',
   },
