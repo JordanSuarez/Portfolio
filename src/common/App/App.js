ba38ab7fa8d33043824ddfee7ae16ca4e9717794
Jordan MacBook <suarez.jordan@orange.fr>
2021-02-26 12:15:40 +0100

    Fix cursor status on page change

--- src/common/App/App.js
+++ src/common/App/App.js
@@ -3,14 +3,11 @@ import React from 'react'
 import {MuiThemeProvider} from '@material-ui/core/styles'
 
 import {routes} from 'common/routing/routes'
-import Cursor from 'common/components/Cursor'
-import IsDevice from 'common/components/Cursor/helpers/IsDevice'
 import Router from 'common/routing/router'
 import theme from 'common/styles/index'
 
 const App = () => (
   <MuiThemeProvider theme={theme}>
-    {typeof navigator !== 'undefined' && IsDevice.any() ? <></> : <Cursor />}
     <Router routes={routes} />
   </MuiThemeProvider>
 )
